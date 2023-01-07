098d85c560b5789f315d49967d93404466079ff5
Jiaqi Zhao (Roy) <72789180+JiaqiZhao2004@users.noreply.github.com>
2023-01-07 17:06:38 +0800

    Update plots.py

--- utils/plots.py
+++ utils/plots.py
@@ -54,9 +54,9 @@ def butter_lowpass_filtfilt(data, cutoff=1500, fs=50000, order=5):
     return filtfilt(b, a, data)  # forward-backward filter
 
 
-def plot_one_box(x, img, color=[185, 136, 108], label=None, line_thickness=1):
+def plot_one_box(x, img, color=[136, 108, 185], label=None, line_thickness=1):
     # Plots one bounding box on image img
-    tl = round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
+    tl = 1 # round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1   line/font thickness
     color = color or [random.randint(0, 255) for _ in range(3)]
     c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
     cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
